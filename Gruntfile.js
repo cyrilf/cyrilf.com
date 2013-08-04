0d23b8e8bb24a711e6dbabefc2c22dd1600d0a93
cyrilf <cyrilf7x@gmail.com>
2013-08-04 01:04:54 -0400

    Boostrap new version. UI imporvements.

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    // Automatically run a task when a file changes
+    watch: {
+      styles: {
+        files: ["css/less/*"],
+        tasks: "less:dev"
+      }
+    },
+
+    // Compile specified less files
+    less: {
+      dev: {
+        options: {
+          // These paths are searched for @imports
+          paths: ["css/less"]
+        },
+        files: {
+          "css/screen.css": "css/less/screen.less"
+        }
+      },
+      prod: {
+        options: {
+          // These paths are searched for @imports
+          paths: ["css/less"],
+          yuicompress: true,
+          report: 'gzip'
+        },
+        files: {
+          "css/screen.css": "css/less/screen.less"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task(s).
+  grunt.registerTask('default', ['less:dev']);
+  grunt.registerTask("dev", ["less:dev", "watch:styles"]);
+  grunt.registerTask("prod", ["less:prod"]);
+};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
+/**
+ * Choose a random background for the header section
+ * and set his properties
+ */
+function randomHeader() {
+  var $header = $('.header');
 
+  if($header.length) {
+    var backgrounds = {
+      0: {
+        url: './img/space.png',
+        style: {
+          background: 'rgba(142, 68, 173, 1) url(./img/space.png) no-repeat right center fixed',
+          'background-size': 'cover'
+        }
+      },
+      1: {
+        url: './img/lake.jpg',
+        style: {
+          background: 'rgba(240, 212, 149, 1) url(./img/lake.jpg) no-repeat center 49%'
+        }
+      },
+      2: {
+        url: './img/mountain.jpg',
+        style: {
+          background: 'rgba(117,125,148, 1) url(./img/mountain.jpg) no-repeat center 30%',
+          'background-size': 'cover'
+        }
+      }
+    };
+
+    var bgCount = Object.keys(backgrounds).length;
+    var random = Math.floor(Math.random()*bgCount);
+
+    var background = backgrounds[random];
+    $header.css(background.style);
+  }
+}
+
+$(function() {
+  randomHeader();
+});
